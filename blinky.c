ade3932b0e5b285bc972851471d5bfa624282b40
Josiah McClurg <jcmcclu2@illinois.edu>
2012-09-21 11:18:25 -0500

    Import repository

---
+++ blinky.c
@@ -0,0 +1,117 @@
+//*****************************************************************************
+//  Blink LED Example
+//
+//  Description: No longer a blinking LED. ;-)
+//*****************************************************************************
+
+/*
+ * ======== Standard MSP430 includes ========
+ */
+#include <msp430.h>
+
+/*
+ * ======== Grace related includes ========
+ */
+#include <ti/mcu/msp430/csl/CSL.h>
+
+/*
+ *  ======== main ========
+ */
+#include "blinky.h"
+#define MAX_VOLTAGE (1024*5/16)
+
+void main(void)
+{
+	stop_watchdog();
+	/*
+     * The MCU crashed and has summarily been reset.
+     * This is considered catastrophic failure since
+     * the whole point of this MCU is to protect more
+     * expensive and complicated circuitry.
+     *
+     * If the MCU ever crashes, it will attempt to
+     * disconnect the server it was protecting, report
+     * the crash to the main supervisory MCU, then
+     * do nothing until a device reset.
+     */
+    // Check if WDT+ caused the prior reset condition
+    if (IFG1 & WDTIFG) {
+    	stop_watchdog();
+        // WDT+ initiated the prior reset condition
+    	on_MCU_crash(); // Handle crash
+        IFG1 &= ~WDTIFG; // Software clear WDTIFG
+    }
+    else
+    {
+    	CSL_init();                     // Activate Grace-generated configuration
+
+		start_adc();
+		start_pwm();
+		set_pwm_duty_cycle(0);
+
+		while (1) {
+			if (STATE == OFFLOADING) {
+				set_pwm_duty_cycle(read_pwm_duty_cycle() + 1);
+
+				// Faster than modulus (and avoids slowness warning)
+				if (read_pwm_duty_cycle() == 100) {
+					STATE = OFF;
+					stop_pwm();
+
+					turn_on_scr();
+				} else {
+					int i;
+					for (i = 0; i < 500; i++) {}
+				}
+			}
+		}
+    }
+}
+
+/*
+ * Interrupt routines for use with the Grace configuration
+ */
+
+// Watchdog Timer: gets called when the MCU
+void on_MCU_crash(void)
+{
+	// Attempt to switch off everything again. Light up "catastrophic failure" LED.
+}
+
+// I2C: Not exactly sure how to implement this yet. Will have to read up on the SMBus protocol.
+void i2c_tx(void)
+{
+}
+
+void i2c_rx(void)
+{
+}
+
+void i2c_start(void)
+{
+}
+
+void i2c_stop(void)
+{
+}
+
+// ADC: gets called when the ADC has a value ready to be read
+void adc_ready(void)
+{
+	if(read_adc() > MAX_VOLTAGE && STATE == STABLE) {
+		STATE = OFFLOADING;
+	}
+}
+
+void overvoltage_condition(void)
+{
+	switch_out_server();
+	slow_short_input_terminals();
+	turn_on_scr();
+}
+
+void slow_short_input_terminals(void)
+{
+	start_pwm();
+	stop_pwm();
+}
---
+++ blinky.h
@@ -0,0 +1,67 @@
+/*
+ * led.h
+ *
+ *  Created on: Sep 15, 2012
+ *      Author: jcmcclu2
+ */
+
+#ifndef LED_H_
+#define LED_H_
+
+typedef enum server_state_enum {
+	STABLE,
+	OFFLOADING,
+	OFF
+} server_state;
+
+server_state STATE = STABLE;
+
+/*
+ * Defines to ease the programming process.
+ */
+// PWM
+#define start_pwm() (TA1CTL |= MC_1) // Start timer in up mode
+#define stop_pwm() (TA1CTL &= ~(MC1 + MC0)) // Clear MCx bits to stop timer
+#define read_pwm_duty_cycle() (TA1CCR1)
+#define set_pwm_duty_cycle(r) (TA1CCR1 = r)
+
+// WATCHDOG
+#define start_watchdog() (WDTCTL = WDTPW)         // Clear hold (and all other) bits
+#define stop_watchdog() (WDTCTL = WDTPW + WDTHOLD) // Set hold bit and clear others
+#define feed_watchdog() (WDTCTL = WDTCTL + WDTCNTCL)  // Set WDT+ counter clear bit (instruction clears other bits)
+
+// ADC
+#define start_adc() (TA0CTL |= MC_1) // Start timer in up mode
+#define stop_adc() (TA0CTL &= ~(MC1 + MC0)) // Clear MCx bits to stop timer
+#define read_adc() (ADC10MEM) // Read ADC conversion result from ADC10MEM
+
+// IO
+#define switch_in_server() (P1OUT |= (BIT4))
+#define switch_out_server() (P1OUT &= ~(BIT4))
+#define turn_on_scr() (P1OUT |= (BIT0))
+
+/*
+ * Interrupt routines for use with the Grace configuration
+ */
+
+// Watchdog Timer
+void on_MCU_crash();
+
+// I2C
+void i2c_tx();
+void i2c_rx();
+void i2c_start();
+void i2c_stop();
+
+// ADC
+void adc_ready();
+
+/*
+ * Our functions
+ */
+
+// High-level
+void overvoltage_condition(void);
+void slow_short_input_terminals(void);
+
+#endif /* LED_H_ */
