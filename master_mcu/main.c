41ac599b50442074bd1a71dc8157cdaeeaabc383
jcmcclurg <jcmcclu2@illinois.edu>
2012-09-22 18:34:33 -0500

    i2c master and uart are now working together!

--- master_mcu/main.c
+++ master_mcu/main.c
@@ -24,8 +24,7 @@ const unsigned char serverAddresses[NUM_SERVERS] = {I2C_SERVER1, I2C_SERVER2, I2
  * Also, it's a generally a bad idea to make multiple sequential calls to send_uart, since the call is
  * non-blocking and will overwrite whatever is already in the buffer.
  */
-char* uartSendPointer = "";
-#define send_uart(x) uartSendPointer = (x); send_byte_uart(*uartSendPointer)
+//#define send_uart(x) uartSendPointer = (x);
 
 // The send_i2c(char* stringToSend, char address) configuration works the same way as send_uart
 char* i2cSendPointer = "";
@@ -44,15 +43,28 @@ void main(int argc, char *argv[])
     CSL_init();                     // Activate Grace-generated configuration
     init_uart();
 
-    green_led_off();
+    top_led_off();
+    bottom_led_off();
     red_led_off();
-
-    green_led_on();
     red_led_on();
+    toggle_red_led();
+
     while(1);
 }
 
 // USART
+void send_uart(char* str)
+{
+	bottom_led_on();
+	while(*str != 0)
+	{
+		while(!(IFG2 & UCA0TXIFG));
+
+		send_byte_uart(*str);
+		str++;
+	}
+	bottom_led_off();
+}
 void init_uart(void)
 {
 	//receiveBuffer[0] = 0;
@@ -61,25 +73,18 @@ void init_uart(void)
 // TODO: Implement circular buffer for storing command before it is parsed
 void uart_rx(void)
 {
+	top_led_on();
 	//*receivePointer = read_byte_uart();
 	char c = read_byte_uart();
 	if(c == 'j')
 	{
-		send_i2c("HI THERE", I2C_SERVER1);
+		send_i2c("HITHERE", I2C_SERVER1);
 	}
 	else
 	{
 	   send_uart("No serial commands have been implemented.\r\n");
 	}
-}
-
-void uart_tx(void)
-{
-	if(*uartSendPointer != 0)
-	{
-		uartSendPointer++;
-		send_byte_uart(*uartSendPointer);
-	}
+	top_led_off();
 }
 
 // I2C
@@ -88,7 +93,7 @@ void i2c_tx()
 	if(*i2cSendPointer != 0)
 	{
 		i2cSendPointer++;
-		send_byte_uart(*i2cSendPointer);
+		toggle_red_led();
 		i2c_tx_byte(*i2cSendPointer);
 	}
 	else
@@ -99,5 +104,5 @@ void i2c_tx()
 
 void i2c_rx()
 {
-
+	bottom_led_off();
 }
--- master_mcu/main.h
+++ master_mcu/main.h
@@ -10,25 +10,36 @@
 
 #define NUM_SERVERS 4
 
-#define green_led_on() (P1OUT &= ~(BIT0))
-#define green_led_off() (P1OUT |= (BIT0))
+#define red_led_off() (P1OUT &= ~(BIT0))
+#define red_led_on() (P1OUT |= (BIT0))
+#define toggle_red_led() (P1OUT ^= BIT0)
 
-#define red_led_on() (P1OUT &= ~(BIT6))
-#define red_led_off() (P1OUT |= (BIT6))
+#define top_led_on() (P1OUT &= ~(BIT4))
+#define top_led_off() (P1OUT |= (BIT4))
+#define toggle_top_led() (P1OUT ^= BIT4)
+
+#define bottom_led_on() (P1OUT &= ~(BIT5))
+#define bottom_led_off() (P1OUT |= (BIT5))
+#define toggle_bottom_led() (P1OUT ^= BIT5)
 
 // USART
 #define read_byte_uart() UCA0RXBUF
 #define send_byte_uart(x) UCA0TXBUF = (x)
+void send_uart(char* str);
 void init_uart(void);
 void uart_rx(void);
-void uart_tx(void);
+//void uart_tx(void);
 
 // I2C
+#define i2c_start() UCB0CTL1 |= UCTXSTT
 #define i2c_start_tx() UCB0CTL1 |= UCTR + UCTXSTT
+
 #define i2c_stop() UCB0CTL1 |= UCTXSTP
 #define i2c_stop_tx() i2c_stop(); IFG2 &= ~UCB0TXIFG
+
 #define i2c_tx_byte(x) UCB0TXBUF = (x)
 #define i2c_set_slave(x) UCB0I2CSA = (x)
+
 #define I2C_SERVER1 1
 #define I2C_SERVER2 2
 #define I2C_SERVER3 3
