dda3ec5159b6b5932185641833119f76189d3c9e
jcmcclurg <jcmcclu2@illinois.edu>
2012-09-22 21:35:22 -0500

    Minor pin assignment changes.

--- master_mcu/main.h
+++ master_mcu/main.h
@@ -10,17 +10,17 @@
 
 #define NUM_SERVERS 4
 
-#define red_led_off() (P1OUT &= ~(BIT0))
-#define red_led_on() (P1OUT |= (BIT0))
-#define toggle_red_led() (P1OUT ^= BIT0)
+#define red_led_off()    (P1OUT &= ~BIT0)
+#define red_led_on()     (P1OUT |=  BIT0)
+#define toggle_red_led() (P1OUT ^=  BIT0)
 
-#define top_led_on() (P1OUT &= ~(BIT4))
-#define top_led_off() (P1OUT |= (BIT4))
-#define toggle_top_led() (P1OUT ^= BIT4)
+#define top_led_on()     (P2OUT &= ~BIT0)
+#define top_led_off()    (P2OUT |=  BIT0)
+#define toggle_top_led() (P2OUT ^=  BIT0)
 
-#define bottom_led_on() (P1OUT &= ~(BIT5))
-#define bottom_led_off() (P1OUT |= (BIT5))
-#define toggle_bottom_led() (P1OUT ^= BIT5)
+#define bottom_led_on()     (P2OUT &= ~BIT1)
+#define bottom_led_off()    (P2OUT |=  BIT1)
+#define toggle_bottom_led() (P2OUT ^=  BIT1)
 
 // USART
 #define read_byte_uart() UCA0RXBUF
